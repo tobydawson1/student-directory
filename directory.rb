a18a85e6ca7b856edd84dafb450654c6a7139a78
Toby Dawson <tobydawson1@gmail.com>
2020-01-13 18:41:04 +0000

    eighth commit - made code more readable

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 #puts all students into an array
 students = [
-  "Dr.Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddie Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 def print_header
 # and then prinnts them individually
@@ -18,11 +18,12 @@ puts "The students of Villains Academy"
 puts "-----------"
 end
 
-def print(names)
-  names.each do |student|
-  puts student
+def print(students)
+  students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer(names)
 # finally, we print the total number of studennts
 puts "Overall, we have #{names.count} great students"
