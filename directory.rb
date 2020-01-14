23493797b7fc9ed83a8bd8c289dbb0e0c120a988
Toby Dawson <tobydawson1@gmail.com>
2020-01-14 13:06:46 +0000

    Ninth Commit - added user input

--- directory.rb
+++ directory.rb
@@ -29,6 +29,25 @@ def print_footer(names)
 puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just his return twice"
+  #reate and empty array
+  students = []
+  #get the first names
+  name = gets.chomp
+  #While name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+students = input_students
 print_header
 print(students)
 print_footer(students)
