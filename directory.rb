dae326dc17cd2e8b5f27aee2bd552b4b7e2257ac
Toby Dawson <tobydawson1@gmail.com>
2020-01-15 17:24:31 +0000

    Tenth Commit - Completeled exercises and refactored code

--- directory.rb
+++ directory.rb
@@ -1,53 +1,98 @@
-#puts all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+@students = []
+
+def interative_menu
+  loop do
+    print_menu
+    # 2. Read the input and save it to a variable
+    # 3. do what the user has asked
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit #this will cause the program to terminate.
+  else
+    puts "I don't know what you meant,try again"
+  end
+end
+def print_menu
+  #1. Print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. EXIT"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def input_students
+  puts "Please enter the names, any hobbies, where they are from and the height of the students"
+  puts "To finish, type stop when asked for a name."
+  #reate and empty array
+  #get the first names
+  puts 'Name?'
+  name = gets.chomp!
+  puts 'Hobbies?'
+  hobbies = gets.chomp!
+  puts 'Where are they from?'
+  origin = gets.chomp!
+  puts 'How tall are they?'
+  height = gets.chomp!
+  #While name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    @students << {name: name, cohort: :november, hobbies: hobbies, COB: origin, height: height }
+    if @students.count == 1
+    puts "Now we have #{@students.count} student"
+    else
+    puts "Now we have #{@students.count} students"
+  end
+    #get another name from the user
+    puts 'Name?'
+    name = gets.chomp!
+    if name == "stop"
+      break
+    end
+    puts 'Hobbies?'
+    hobbies = gets.chomp!
+    puts 'Where are they from?'
+    origin = gets.chomp!
+    puts 'How tall are they?'
+    height = gets.chomp!
+  end
+end
+
 def print_header
 # and then prinnts them individually
 puts "The students of Villains Academy"
 puts "-----------"
 end
 
-def print(students)
-  students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
+def print_student_list
+  @students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort) (#{student[:hobbies]} is his hobbie ) (From #{student[:COB]}) ( and his height is #{student[:height]}) "
+end
 end
 
-def print_footer(names)
+def print_footer
 # finally, we print the total number of studennts
-puts "Overall, we have #{names.count} great students"
+if @students.count == 1
+  puts "Overall, we have #{@students.count} great student"
+else
+  puts "Overall, we have #{@students.count} great students"
+end
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just his return twice"
-  #reate and empty array
-  students = []
-  #get the first names
-  name = gets.chomp
-  #While name is not empty, repeat this code
-  while !name.empty? do
-    #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    #get another name from the user
-    name = gets.chomp
-  end
-  #return the array of students
-  students
-end
-students = input_students
+interative_menu
 print_header
-print(students)
-print_footer(students)
+print
+print_footer
